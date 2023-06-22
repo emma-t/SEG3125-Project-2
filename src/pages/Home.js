58622f661593f5fbf8358673d41a3ab5dd551f28
Emma <emma.tangg@gmail.com>
2023-06-21 23:37:27 -0400

    Created nav and base home page

---
+++ src/pages/Home.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Home(){
+
+    return(
+
+        <div>
+            <h2 className="text-center">Ottawa</h2>
+            <p className="text-center">
+            Lorem ipsum odor amet, consectetuer adipiscing elit. Ac purus in massa egestas mollis varius;
+                                        dignissim elementum. Mollis tincidunt mattis hendrerit dolor eros enim, nisi ligula ornare.
+                                        Hendrerit parturient habitant pharetra rutrum gravida porttitor eros feugiat. Mollis elit
+                                        sodales taciti duis praesent id. Consequat urna vitae morbi nunc congue.
+            </p>
+            
+        </div>
+
+    );
+
+}
+
+export default Home;
