7126e7d3f0885a3f16708add9f1bb8e44a046928
Emma <emma.tangg@gmail.com>
2023-07-02 18:03:15 -0400

    NavBar updated for links

--- src/components/PageNavbar.js
+++ src/components/PageNavbar.js
@@ -3,21 +3,21 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 function PageNavbar() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand as={Link} to="/">ReviveMart</Navbar.Brand>
+        <Navbar.Brand as={NavLink} to="/">ReviveMart</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link as={Link} to="/donate">Donate</Nav.Link>
-            <Nav.Link as={Link} to="/items">Our Items</Nav.Link>
-            <Nav.Link as={Link} to="/about">About Us</Nav.Link>
-            <Nav.Link as={Link} to="/faq-reviews">FAQ & Reviews</Nav.Link>
-            <Nav.Link as={Link} to="/contact">Contact Us</Nav.Link>
+            <Nav.Link as={NavLink} to="/donate">Donate</Nav.Link>
+            <Nav.Link as={NavLink} to="/items">Our Items</Nav.Link>
+            <Nav.Link as={NavLink} to="/about">About Us</Nav.Link>
+            <Nav.Link as={NavLink} to="/faq-reviews">FAQ & Reviews</Nav.Link>
+            <Nav.Link as={NavLink} to="/contact">Contact Us</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
