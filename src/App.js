ae54a6fcd09676ffcb7d06bed2cbe5ec38d9a61a
Emma <emma.tangg@gmail.com>
2023-07-02 16:32:23 -0400

    More url fixing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
 import PageNavbar from './components/PageNavbar';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Donate from './pages/Donate';
-import Info from './pages/Info.js';
-import Confirm from './pages/Confirm.js';
+import Info from './pages/Info';
+import Confirm from './pages/Confirm';
 import Items from './pages/Items';
 import About from './pages/About';
 import FAQReviews from './pages/FAQReviews';
@@ -12,29 +12,20 @@ import Contact from './pages/Contact';
 
 function App() {
   return (
-    <Router>
-
+    <Router basename="/seg3125-project-2">
       <div>
-      <PageNavbar />
-
-      <Routes>
-        <Route path='/' element = {<Home />} />
-        <Route path='/donate' element = {<Donate />} />
-        <Route path='/info' element = {<Info />} />
-        <Route path='/confirm' element = {<Confirm />} />
-        <Route path='/items' element = {<Items />} />
-        <Route path='/about' element = {<About />} />
-        <Route path='/faq-reviews' element = {<FAQReviews />} />
-        <Route path='/contact' element = {<Contact />} />
-        {/* <Route path='/Services' element = {<Services />} /> */}
-        {/* <Route path='/Aboutus' element = {<Aboutus />} /> */}
-
-      </Routes>
-
-
+        <PageNavbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/donate" element={<Donate />} />
+          <Route path="/info" element={<Info />} />
+          <Route path="/confirm" element={<Confirm />} />
+          <Route path="/items" element={<Items />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/faq-reviews" element={<FAQReviews />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
       </div>
-      
-
     </Router>
   );
 }
--- src/components/PageNavbar.js
+++ src/components/PageNavbar.js
@@ -3,21 +3,21 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import { Link } from 'react-router-dom';
 
 function PageNavbar() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="/">ReviveMart</Navbar.Brand>
+        <Navbar.Brand as={Link} to="/">ReviveMart</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            {/* <Nav.Link href="/">Home</Nav.Link> */}
-            <Nav.Link href="/donate">Donate</Nav.Link>
-            <Nav.Link href="/items">Our Items</Nav.Link>
-            <Nav.Link href="/about">About Us</Nav.Link>
-            <Nav.Link href="/faq-reviews">FAQ & Reviews</Nav.Link>
-            <Nav.Link href="/contact">Contact Us</Nav.Link>
+            <Nav.Link as={Link} to="/donate">Donate</Nav.Link>
+            <Nav.Link as={Link} to="/items">Our Items</Nav.Link>
+            <Nav.Link as={Link} to="/about">About Us</Nav.Link>
+            <Nav.Link as={Link} to="/faq-reviews">FAQ & Reviews</Nav.Link>
+            <Nav.Link as={Link} to="/contact">Contact Us</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
